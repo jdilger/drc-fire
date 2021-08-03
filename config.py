56f92c78475fa5017e951aa6c4de70aaf16fce34
John Dilger <johnjdilger@gmail.com>
2021-08-03 17:13:35 -0500

    added export path to old config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # // the default path to use if a pth isnt set in the landcoverOptions function.
 # // currently has export paths for default forest types for testing. 
-defaultExportPath = "projects/goldmine-265915/assets/drc_fire/test_runs"
+defaultExportPath = "projects/sig-misc-ee/assets/drc_fire/test_runs"
 
 
 coverDict = {
@@ -8,25 +8,25 @@ coverDict = {
       "value":1,
       "abbreviation": "DHF",
       "color":'8aa745',
-      "exportPath":"projects/goldmine-265915/assets/drc_fire/test_runs/dhf"
+      "exportPath":"projects/sig-misc-ee/assets/drc_fire/test_runs/dhf"
     },
     "Dense moist forest on hydromorphic soil":{
       "value":2,
       "abbreviation":"DMF",
       "color":'6ee682',
-      "exportPath":"projects/goldmine-265915/assets/drc_fire/test_runs/dmf"
+      "exportPath":"projects/sig-misc-ee/assets/drc_fire/test_runs/dmf"
     },
     "Secondary forest":{
       "value":3,
       "abbreviation":"SNDF",
       "color":'f0ff72',
-      "exportPath": "projects/goldmine-265915/assets/drc_fire/test_runs/sndf"
+      "exportPath": "projects/sig-misc-ee/assets/drc_fire/test_runs/sndf"
     },
     "Dry forest or open forest":{
       "value":4,
       "abbreviation":"DRYF",
       "color":'ffc625',
-      "exportPath":"projects/goldmine-265915/assets/drc_fire/test_runs/dryf"
+      "exportPath":"projects/sig-misc-ee/assets/drc_fire/test_runs/dryf"
     },
     "Savannah":{
       "value":5,
@@ -56,7 +56,7 @@ coverDict = {
     "Forests without dry forest":{
       "value": [1,2,3],
       "abbreviation":"FOREST",
-      "exportPath":"projects/goldmine-265915/assets/drc_fire/test_runs/forests"
+      "exportPath":"projects/sig-misc-ee/assets/drc_fire/test_runs/forests"
     }
 
   }
