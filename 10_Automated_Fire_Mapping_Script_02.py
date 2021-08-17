bf882a4ef1b70ea96608d59288aeeac133ce46ed
John Dilger <johnjdilger@gmail.com>
2021-08-16 23:12:28 -0500

    added export and md info

--- 10_Automated_Fire_Mapping_Script_02.py
+++ 10_Automated_Fire_Mapping_Script_02.py
@@ -2,13 +2,28 @@
 import ee
 
 ee.Initialize()
-import config as c
-from paramters import *
-
-
+import fire_params
+pio = fire_params.paramtersIO()
+    # # // The land cover map is for 2000.
+    # # // Cover classes are in this order:
+    # # // Value, Label, Abbreviation
+    # # // "1", "Dense humid forest on dry land", DHF
+    # # // "2" ,"Dense moist forest on hydromorphic soil", DMF
+    # # // "3" ,"Secondary forest", SNDF
+    # # // "4" ,"Dry forest or open forest", DRYF
+    # # // "5" ,"Savannah", SAV
+    # # // "6" ,"Cultures and regeneration of abandoned crops", REGN
+    # # // "7" ,"Water zone", WATER
+    # # // "8" ,"Agglomeration", AGGL
+    # # // "9" ,"Other", OTHER
+    # # // "1,2,3" ,"Forests without dry forest", FOREST
+    # # //Chose a cover class to filter to for fire detection:
+covername = "Forests without dry forest"
+print(dir(pio))
+coverDict = pio.coverDict
 
 # // Import and sort image collection of z scores
-zCollection = 'projects/sig-misc-ee/assets/drc_fire/test_runs/forests_python'
+zCollection = coverDict[covername]["exportPath"]
 
 #   ///////////////////////////////////////////////////////////////////////
 #  ////////// 2. Parameters //////////////////////////////////////////////
@@ -21,9 +36,30 @@ alpha = 0.05
 pVal = 'pval_spatial' 
 
 # // Location for Asset exports
-exportCollection =  'projects/ee-karistenneson/assets/BurnedBiomass/DRC_Exports/DRC_BurnMaps_2016'
+exportCollection =   coverDict[covername]['exportPathYearly']
+templateImage = zScores.first()
+studyArea = templateImage.geometry()
+templateProjection = templateImage.projection()
+scale = templateProjection.nominalScale().getInfo()
+crs = templateProjection.crs().getInfo()
+analysisYear = templateImage.get('analysisYear').getInfo()
+coverName = templateImage.get('coverName').getInfo()
+burnYearly = joinZPandPPZP.select(['burn','yearMonthDay']).max()
+# // Get metadata about collection from the first image
+nonSysProperties = templateImage.propertyNames()
+metaData = templateImage.toDictionary(nonSysProperties.cat(['system:asset_size','system:time_start','system:footprint']))
 
-zScores = ee.ImageCollection(zCollection).sort('system:time_start')
+# // Add land cover band back for export
+cover = cover = ee.Image('projects/sig-ee/FIRE/DRC/DIAF_2000forest').rename('landcover')
+
+def scalePBands(img:ee.Image)->ee.Image:
+  s_bands = img.select(["pval_spatial",'pval_temporal'])
+  s_bands = s_bands.divide(10000)
+  return img.select('N').addBands(s_bands)
+
+zScores = ee.ImageCollection(zCollection) \
+          .sort('system:time_start') \
+            .map(scalePBands)
 
 def zeroPad(n):
     return n.format('%02d')
@@ -146,21 +182,39 @@ zScoresBurnMasked = zScores.map(ppBurnDate)
 # # // Join the collections - take only what you need
 joinZPandPPZP = joinCollections(zScoresTHandMasked.select(["burn","yearMonthDay","month","day"]), zScoresBurnMasked, False)
 print(joinZPandPPZP.first().bandNames().getInfo())
-# # // Remove the mean, stdDev, and N bands
-joinZPandPPZP = joinZPandPPZP.select([0,1,2,3,4,5,6,7])
-print(joinZPandPPZP.first().bandNames().getInfo())
-# print('Here is the final burn map collection for export:', 
-# joinZPandPPZP.size().getInfo(),joinZPandPPZP.first().bandNames().getInfo())
 
+# //EXPORT BURN MAPS
+# // Get export parameters from a template image (the first) in the collection
 
-# test zero padding
-img = zScores.first()
+forExport = ee.Image.cat([burnYearly,cover]).set(metaData)
 
-# d = ee.Date(img.get('system:time_start'))
-# endDate = d.advance(ee.Number(analysisPeriod).divide(2),'day')
-# startDate = ee.Date.fromYMD(d.get('year'),1,1)
-# print(startDate.get('year').getInfo(),startDate.get('month').getInfo(),startDate.get('day').getInfo())
-# print(getYearMonthDay(startDate.get('year'),startDate.get('month'),startDate.get('day')).getInfo())
-# print(zeroPad(startDate.get('month')).getInfo())
-# startYMD = ee.Number(getYearMonthDay(startDate.get('year'),startDate.get('month'),startDate.get('day')))
-# endYMD = ee.Number(getYearMonthDay(endDate.get('year'),endDate.get('month'),endDate.get('day')))
+print(forExport.bandNames().getInfo())
+
+def export_burn_yearly(image, geometry, export_path=None,exportScale=None,crs=None,test=False):
+  
+    if exportScale is None:exportScale = exportScale
+    if crs is None: crs = crs
+    if export_path is None: export_path = coverDict[coverName]["exportPath"]
+
+    imgName = f"burn_{analysisYear}_{coverDict[coverName]['abbreviation']}"
+    print('imgName:',imgName)
+    
+    if test:
+        print(image.toDictionary().getInfo())
+        print(ee.Image(image).bandNames().getInfo())
+        print(f'{export_path}/{imgName}')
+        print('export setting:','exportScale',exportScale,'crs',crs)
+    else:
+        task = ee.batch.Export.image.toAsset(
+            image=image,description=imgName,
+            assetId=f'{export_path}/{imgName}',
+            region=geometry,
+            scale=exportScale,
+            crs=crs,
+            maxPixels=1e13,
+            )
+        
+        task.start()
+    pass
+
+export_burn_yearly(forExport, studyArea,exportCollection,scale,crs,test=False)
